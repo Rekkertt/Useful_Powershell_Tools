e5d7129a23838242b9b14554653f9af5797ab3fa
Rekkertt <115654922+Rekkertt@users.noreply.github.com>
2024-10-23 00:53:27 +0200

    Update ConvertTools.psm1

--- ConvertTools/ConvertTools.psm1
+++ ConvertTools/ConvertTools.psm1
@@ -1,5 +1,27 @@
 Function ConvertFrom-Base64 {  
 
+<#
+.SYNOPSIS
+Converts a Base64 encoded string to its original string representation.
+
+.DESCRIPTION
+The ConvertFrom-Base64 function decodes a Base64 encoded string back to its original string format using the specified encoding.
+
+.PARAMETER Encoding
+The encoding type to use for the conversion. Valid values are "UTF8", "Unicode", and "ASCII".
+
+.PARAMETER InputString
+The Base64 encoded string that needs to be decoded.
+
+.EXAMPLE
+PS C:\> ConvertFrom-Base64 -Encoding "UTF8" -InputString "SGVsbG8gd29ybGQ="
+This command converts the Base64 encoded string "SGVsbG8gd29ybGQ=" to its original UTF8 string "Hello world".
+
+.NOTES
+Author: Your Name
+Date: YYYY-MM-DD
+#>
+
     [CmdletBinding()]
     param
     (    
