725181445ae48e0518b8486d191f5217846f1168
Rekkertt <115654922+Rekkertt@users.noreply.github.com>
2024-10-23 00:51:53 +0200

    Update ConvertTools.psm1

--- ConvertTools/ConvertTools.psm1
+++ ConvertTools/ConvertTools.psm1
@@ -16,7 +16,26 @@ Function ConvertFrom-Base64 {
 
 
 Function ConvertTo-Base64($path) {
-	
+
+<#
+.SYNOPSIS
+Converts the contents of a file to a Base64 encoded string.
+
+.DESCRIPTION
+The ConvertTo-Base64 function reads the contents of a specified file and converts it to a Base64 encoded string using .NET's Convert class.
+
+.PARAMETER path
+The path to the file that needs to be converted to a Base64 string.
+
+.EXAMPLE
+PS C:\> ConvertTo-Base64 -path "C:\example\file.txt"
+This command converts the contents of "file.txt" to a Base64 encoded string.
+
+.NOTES
+Author: Rekkertt
+Date: 23-10-2024
+#>
+ 
     return [Convert]::ToBase64String((Get-Content $path -Encoding byte))
 }
 
